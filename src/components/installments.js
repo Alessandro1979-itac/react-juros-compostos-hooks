088ebc2b96ca5209633fb169e4cdd99e8f192f65
Alessandro1979-itac <muniz.caranha@gmail.com>
2020-06-18 21:13:10 -0400

    Correção de um erro de digitação

--- src/components/installments.js
+++ src/components/installments.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Installment from './Installment';
 
-export default function Installments({ Installments }) {
+export default function Installments({ installments }) {
   return (
     <div className="row">
-      {Installments.map((installment) => {
+      {installments.map((installment) => {
         return <Installment key={installment.id} installment={installment} />;
       })}
     </div>
