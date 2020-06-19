0626272fa87f3b98e2484d25c56d420205bdb67b
Alessandro1979-itac <muniz.caranha@gmail.com>
2020-06-18 20:56:16 -0400

    Adicionado o arquivo App.js em src com toda lógica

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import Installments from './components/Installments';
+import Form from './components/Form';
+
+export default function App() {
+  const [initialValue, setInitialValue] = useState(1000);
+  const [monthlyInterest, setMonthlyInterest] = useState(0.5);
+  const [monthlyPeriod, setMonthlyPeriod] = useState(1);
+  const [installments, setInstallments] = useState([]);
+
+  const handleFormChanges = (newValue, newInterest, newPeriod) => {
+    if (newValue !== null) {
+      setInitialValue(newValue);
+      return;
+    }
+
+    if (newInterest !== null) {
+      setMonthlyInterest(newInterest);
+      return;
+    }
+
+    setMonthlyPeriod(newPeriod);
+  };
+
+  useEffect(() => {
+    calculateInterest(initialValue, monthlyInterest, monthlyPeriod);
+  }, [initialValue, monthlyInterest, monthlyPeriod]);
+
+  const calculateInterest = (initialValue, setMonthlyInterest, monthlyPeriod) => {
+    const newInstallments = [];
+    
+    let currentId = 1;
+    let currentValue = initialValue;
+    let percentage = 0;
+
+    for(let i = 1; i <= monthlyPeriod; i++) {
+      const percentValue = (currentValue * Math.abs(setMonthlyInterest)) / 100;
+
+      currentValue = monthlyInterest >= 0
+        ? currentValue + percentValue
+        : currentValue - percentValue;
+      
+      percentage = (currentValue / initialValue - 1) * 100;
+
+      newInstallments.push({
+        id: currentId++,
+        value: currentValue,
+        difference: currentValue - initialValue,
+        percentage,
+        profit: monthlyInterest > 0,
+      });
+    }
+    setInstallments(newInstallments);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container center">
+      <h1 className="center">React - Juros Compostos</h1>
+
+      <Form
+        data={{ initialValue, monthlyInterest, monthlyPeriod }}
+        onChangeData={handleFormChanges}
+      />
+
+      <div className="center">
+        <Installments installments={installments} />
+      </div>
     </div>
   );
 }
-
-export default App;
